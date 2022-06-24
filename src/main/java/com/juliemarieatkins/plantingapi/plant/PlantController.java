9e043c5e10bfb37ca656cdfb1dfd510949a9be65
Julie <jatkin80@ford.com>
2022-06-24 14:54:09 -0500

    add functionality

--- src/main/java/com/juliemarieatkins/plantingapi/plant/PlantController.java
+++ src/main/java/com/juliemarieatkins/plantingapi/plant/PlantController.java
@@ -1,15 +1,15 @@
 package com.juliemarieatkins.plantingapi.plant;
 
 import java.util.Map;
-import java.util.HashMap;
-
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @CrossOrigin
@@ -21,15 +21,13 @@ public class PlantController {
 
   @GetMapping
   @ResponseStatus(HttpStatus.OK)
-  public Map<String, Iterable<Plant>> list(){
-    Iterable<Plant> plants = plantService.list();
-    return createHashPlural(plants);
+  public Map<String, Iterable<Plant>> list() {
+    return plantService.list();
   }
 
-  private Map<String, Iterable<Plant>> createHashPlural(Iterable<Plant> plants){
-    Map<String, Iterable<Plant>> response = new HashMap<String, Iterable<Plant>>();
-    response.put("plants", plants);
-
-    return response;
+  @PostMapping
+  @ResponseStatus(HttpStatus.CREATED)
+  public Map<String, Plant> create(@Validated @RequestBody Plant plant) {
+return plantService.create(plant);
   }
 }
--- src/main/java/com/juliemarieatkins/plantingapi/plant/PlantService.java
+++ src/main/java/com/juliemarieatkins/plantingapi/plant/PlantService.java
@@ -1,5 +1,7 @@
 package com.juliemarieatkins.plantingapi.plant;
 
+import java.util.HashMap;
+import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -8,7 +10,27 @@ public class PlantService {
   @Autowired
   private PlantRepository plantRepository;
 
-  public Iterable<Plant> list(){
-    return plantRepository.findAll();
+  public Map<String, Iterable<Plant>> list(){
+Iterable<Plant> plants=plantRepository.findAll();
+    return createHashPlural(plants);
   }
+
+  public Map<String, Plant>create(Plant plant) {
+  Plant savedPlant= plantRepository.save(plant);
+  return createHashSingular(savedPlant);
+  }
+
+  private Map<String, Plant> createHashSingular(Plant plant){
+  Map<String, Plant> response=new HashMap<String, Plant>();
+  response.put("plant", plant);
+
+  return response;
+  }
+
+  private Map<String, Iterable<Plant>>createHashPlural(Iterable<Plant> plants){
+Map<String, Iterable <Plant>>response= new HashMap<String, Iterable<Plant>>();
+response.put("plants", plants);
+return response;
+  }
+
 }
